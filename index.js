98977ec7c5148d26f04883364c704487e1255816
Salmaan <salmaan.arif.ali@gmail.com>
2021-07-06 23:28:25 -0400

    commit tree fd54b3650db37ebee688d92f1394d5d35e8b0d75

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Your code here
+function mapToNegativize(sourceArray) {
+  let newArray = [];
+  sourceArray.forEach( element => {
+    newArray.push(element * -1 );  
+  });
+  return newArray;
+}
+
+function mapToNoChange(sourceArray) {
+  let newArray = [];
+  sourceArray.forEach( element => {
+    newArray.push(element);
+  });
+  return newArray;
+}
+
+function mapToDouble(sourceArray) {
+  let newArray = [];
+  sourceArray.forEach( element => {
+    newArray.push(element * 2);
+  });
+  return newArray;
+}
+
+function mapToSquare(sourceArray) {
+  let newArray = [];
+  sourceArray.forEach( element => {
+    newArray.push(element ** 2)
+  });
+  return newArray;
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0) {
+  let sum = startingPoint;
+  sourceArray.forEach( element => {
+    sum += element;
+  });
+  return sum;
+}
+
+function reduceToAllTrue(sourceArray) {
+  let result = true;
+  sourceArray.forEach( element => {
+    if (element === false) { result = false };
+  });
+  return result;
+}
+
+function reduceToAnyTrue(sourceArray) {
+  let result = false;
+  sourceArray.forEach( element => {
+    if (element === true) { result = true };
+  });
+  return result;
+}
